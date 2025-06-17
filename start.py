efb3e9df2fcf8c5eb33f0496846bdc25b5de6499
Uniuxify <Uniuxify@gmail.com>
2025-06-17 07:59:04 +0300

    refs/heads

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from types import ModuleType
 
-with urlopen('https://raw.githubusercontent.com/uniuxify/nlp-help/main/main.py') as resp:
+with urlopen('https://raw.githubusercontent.com/uniuxify/nlp-help/refs/heads/main/main.py') as resp:
   src = (resp.read().decode())
 
 compiled = compile(src, '', 'exec')
